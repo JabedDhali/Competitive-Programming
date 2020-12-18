9e6fb716f137836c0738fd78dfccda58f93a7293
JabedDhali <62301709+JabedDhali@users.noreply.github.com>
2020-12-18 11:21:59 +0600

    Create 1455A Strange Functions.cpp

---
+++ Codeforces/1455A Strange Functions.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    int tc;
+    cin>>tc;
+    while(tc--){
+        cin>>str;
+        cout<<str.size()<<endl;
+    }
+}
